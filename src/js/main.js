65abf0871ac5f4ba629c94732fdc341d6bd41b70
Kyle Turco <kyleturco@gmail.com>
2015-11-29 23:04:20 -0600

    add jquery, add first save function

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-// main.js
+var ref = 'https://turco-groceries.firebaseio.com/';
+
+$('.btn-default').on('click', function () {
+  ref.push({
+    item: $('.add-grocery').val()
+  });
+  $('.add-grocery').val('');
+  console.log("this shit worked");
+});
