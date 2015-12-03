9baf625b5a37a0d987f5609e7a6c901cf058dea8
Kyle Turco <kyleturco@gmail.com>
2015-12-02 21:54:17 -0600

    display items on page load

--- src/js/main.js
+++ src/js/main.js
@@ -18,28 +18,29 @@ toastr.options = {
 
 var ref = new Firebase('https://turco-groceries.firebaseio.com/groceries');
 
+// Saves the grocery item to Firebase
+// Calls the function to display it on the page
 $('.grocery-submit').on('click', function() {
   if ($('#add-grocery-input').val().length == 0) {
     toastr["error"]("Error: Please enter a grocery item")
   } else {
     var input = $('#add-grocery-input').val();
     var newInput = input.charAt(0).toUpperCase() + input.slice(1);
-    console.log(newInput);
     ref.push({
       item: newInput,
     });
     $('#add-grocery-input').val('');
-  }
+    $('.single-grocery-item').append("<div>" + newInput + "</div>");  }
 });
 
-// var itemDisplay = $('#single-grocery-text');
-// var itemInput = $('#add-grocery-input');
-// var submitBtn = $('.grocery-submit');
 
-ref.once("value", function(allGroceriesSnapshot) {
-  allGroceriesSnapshot.forEach(function(grocerySnapshot) {
-    var key = grocerySnapshot.key();
-    var item = grocerySnapshot.child("item").val();
-    $('.single-grocery-item').append("<div>" + item + "</div>");
-  })
-});
+// Adds the grocery items to the page
+window.onload = function() {
+  ref.once("value", function(allGroceriesSnapshot) {
+    allGroceriesSnapshot.forEach(function(grocerySnapshot) {
+      var key = grocerySnapshot.key();
+      var item = grocerySnapshot.child("item").val();
+      $('.single-grocery-item').append("<div>" + item + "</div>");
+    })
+  });
+}
