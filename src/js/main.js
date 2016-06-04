0ac8d649c08700816cd4c560c6e72076bc47fada
Kyle Turco <kyleturco@gmail.com>
2016-06-04 14:39:15 -0500

    feat(App): retrive FB key for clicked item

--- src/js/main.js
+++ src/js/main.js
@@ -5,25 +5,37 @@ jQuery(document).ready(function($) {
   var $grocerySubmit = $('.grocery-submit')
   var $groceryInput = $('#add-grocery-input')
   var $singleGrocery = $('.single-grocery-item')
+  var $deleteBtn = $('#delete-btn')
+
+  var deleteBtn = '<div class="btn btn-default" id="delete-btn">X</div>'
 
   function render() {
     $singleGrocery.empty()
-    
+
     ref.once('value', function(allGroceriesSnapshot) {
       allGroceriesSnapshot.forEach(function(grocerySnapshot) {
         var key = grocerySnapshot.key()
         var item = grocerySnapshot.child("groceryItem").val()
-        $singleGrocery.append('<div>' + key + ': ' + item + '</div>')
+        $singleGrocery.append('<div class="grocery-item ' + key + '">' + item + deleteBtn + '</div>')
       })
     })
   }
 
-  function addGrocery() {
+  function addGrocery(ref) {
     ref.push({
       groceryItem: $groceryInput.val()
     })
     $groceryInput.val('')
     render()
+  }
+
+
+  $(document).on('click', '#delete-btn', function(e) {
+    getFbKey(e)
+  })
+
+  function getFbKey(e) {
+    console.log($(e.currentTarget).parent().attr('class'))
   }
 
   $grocerySubmit.on('click', addGrocery)
