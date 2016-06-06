6fac974e001d09ec8333b4421e1f6e0d99dfd854
Kyle Turco <kyleturco@gmail.com>
2016-06-06 01:17:47 -0500

    chore(App): remove vars, update for const

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
 jQuery(document).ready(function($) {
   // Sets up FB ref
-  var ref = new Firebase('https://turco-groceries.firebaseio.com/groceries/')
+  const ref = new Firebase('https://turco-groceries.firebaseio.com/groceries/')
 
   // DOM elements
-  var $grocerySubmit = $('.grocery-submit')
-  var $groceryInput = $('#add-grocery-input')
-  var $singleGrocery = $('.single-grocery-item')
-  var $deleteBtn = $('.delete-btn')
-  var $document = $(document)
+  const $grocerySubmit = $('.grocery-submit')
+  const $groceryInput = $('#add-grocery-input')
+  const $singleGrocery = $('.single-grocery-item')
+  const $deleteBtn = $('.delete-btn')
+  const $document = $(document)
 
-  var deleteBtn = '<div class="btn delete-btn"><i class="fa fa-times-circle" aria-hidden="true"></i></div>'
+  const deleteBtn = '<div class="btn delete-btn"><i class="fa fa-times-circle" aria-hidden="true"></i></div>'
 
   function render() {
     $singleGrocery.empty()
 
     ref.once('value', function(allGroceriesSnapshot) {
       allGroceriesSnapshot.forEach(function(grocerySnapshot) {
-        var key = grocerySnapshot.key()
-        var item = grocerySnapshot.child("groceryItem").val()
+        const key = grocerySnapshot.key()
+        const item = grocerySnapshot.child("groceryItem").val()
         $singleGrocery.append('<div class="grocery-item ' + key + '">' + item + deleteBtn + '</div>')
       })
     })
@@ -38,8 +38,8 @@ jQuery(document).ready(function($) {
   }
 
   function removeGroceryItem(e) {
-    var groceryFbKey = $(e.currentTarget).parent().attr('class').split(' ').slice(1)
-    var groceryItemRef = new Firebase(ref + '/' + groceryFbKey)
+    const groceryFbKey = $(e.currentTarget).parent().attr('class').split(' ').slice(1)
+    const groceryItemRef = new Firebase(ref + '/' + groceryFbKey)
     groceryItemRef.remove(function(error) {
       if (error) {
         console.log("Error;", error);
